973f50ea074b6e7924594f109eff63f500e8916d
vitorakutsu <vitorakutsu@gmail.com>
2024-11-15 00:29:25 -0300

    feat: added mymovies tests class

---
+++ src/test/java/org/application/mymovies/MyMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.application.mymovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
