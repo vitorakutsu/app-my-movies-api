0b207067bac2111514971a9aea494fbb4728f3b1
vitorakutsu <vitorakutsu@gmail.com>
2024-11-15 00:29:20 -0300

    feat: added mymovies main class

---
+++ src/main/java/org/application/mymovies/MyMoviesApplication.java
@@ -0,0 +1,13 @@
+package org.application.mymovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMoviesApplication.class, args);
+	}
+
+}
