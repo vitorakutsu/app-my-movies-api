54615bd317a1d2b9249384e68e96e40b45aa1432
vitorakutsu <vitorakutsu@gmail.com>
2024-11-15 00:29:00 -0300

    feat: added movie entity

---
+++ src/main/java/org/application/mymovies/entities/Movie.java
@@ -0,0 +1,16 @@
+package org.application.mymovies.entities;
+
+import java.util.ArrayList;
+
+public class Movie{
+    public String title;
+    public int year;
+    public ArrayList<String> cast;
+    public ArrayList<String> genres;
+    public String href;
+    public String extract;
+    public String thumbnail;
+    public int thumbnail_width;
+    public int thumbnail_height;
+}
+
